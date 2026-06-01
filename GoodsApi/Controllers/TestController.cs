371ef1dbfe7fcb18dcc3ca346d7f5b7d6e625598
6ECCMEPTHIoIIJ <shamraev.alexandr.github@gmail.com>
2026-06-01 20:57:46 +0300

    Разделение Docker compose на Development и Production. Тестовый контроллер

---
+++ GoodsApi/Controllers/TestController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace GoodsApi.Controllers;
+
+[ApiController]
+[Route("api")]
+public class TestController : ControllerBase
+{
+    [HttpGet("hello")]
+    public async Task<ActionResult<string>> Get()
+    {
+        return "Hello World!";
+    }
+}
--- GoodsApi/Program.cs
+++ GoodsApi/Program.cs
@@ -1,48 +1,39 @@
 namespace GoodsApi;
 
-public class Program
+public static class Program
 {
-    public static void Main(string[] args)
+    public static async Task Main(string[] args)
     {
         var builder = WebApplication.CreateBuilder(args);
 
-        // Add services to the container.
+        #region Services
+
+        builder.Services.AddControllers();
         builder.Services.AddAuthorization();
 
-        // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
+        #endregion
+        
+        #region API Docs
+
         builder.Services.AddOpenApi();
+        builder.Services.AddEndpointsApiExplorer();
+        builder.Services.AddSwaggerGen();
+
+        #endregion
 
         var app = builder.Build();
 
-        // Configure the HTTP request pipeline.
         if (app.Environment.IsDevelopment())
         {
             app.MapOpenApi();
+            app.UseSwagger();
+            app.UseSwaggerUI();
         }
 
         app.UseHttpsRedirection();
-
+        app.MapControllers();
         app.UseAuthorization();
 
-        var summaries = new[]
-        {
-            "Freezing", "Bracing", "Chilly", "Cool", "Mild", "Warm", "Balmy", "Hot", "Sweltering", "Scorching"
-        };
-
-        app.MapGet("/weatherforecast", (HttpContext httpContext) =>
-            {
-                var forecast = Enumerable.Range(1, 5).Select(index =>
-                        new WeatherForecast
-                        {
-                            Date = DateOnly.FromDateTime(DateTime.Now.AddDays(index)),
-                            TemperatureC = Random.Shared.Next(-20, 55),
-                            Summary = summaries[Random.Shared.Next(summaries.Length)]
-                        })
-                    .ToArray();
-                return forecast;
-            })
-            .WithName("GetWeatherForecast");
-
-        app.Run();
+        await app.RunAsync();
     }
 }
